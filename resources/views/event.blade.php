8acffb023fcebb6d82066055e1a89b276acaa2cf
Ndeme <y.ndeme@sevendadvancedacademy.com>
2020-07-14 09:09:31 +0100

    push init

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -11,4 +11,8 @@
     @section('footer')
         Ndeme Yvan &copy 2020
     @endsection
+
+    @section('script.footer')
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    @endsection
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,15 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title or "Boom" }}</title>
     {{-- le OR me permet de dire a blade que si une valeur n'est pas passe lors de l'extentiation de blade met plutot BOOM a la place --}}
+    {{-- On peut aussi utiliser  ?? si on utilise php version 7 et plus --}}
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body>
-    @include('layouts.partials._nav')
+    @include('layouts.partials._nav',["age"=>"25"])
+
+    {{-- les partials peuvents aussi transporter des variables --}}
+    {{-- En consequent le partial en question a aussi acces a toutes les variables du layout ou il est inclu --}}
+    {{-- le @{{ }} dit a blade de ne pas interpreter un code qui peut etre utiliser par un autre framework js comme vuJs --}}
+    {{-- On peut ausi utiliser notre code a linterieur d'un bloc @verbatim @endvarbatim au cas ou on as un bloc de code a ne pas interpreter par blade --}}
+
     @yield('content')
 
     <footer>
         @yield('footer')
     </footer>
+
+    @yield('script.footer')
 
 </body>
 </html>
